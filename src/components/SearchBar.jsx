5976c32f5c5399550afc2a8d31a028d8f258c61f
Geetisha Tandon <93433422+Geetisha17@users.noreply.github.com>
2025-10-19 16:57:32 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ export default function Searchbar()
             placeholder="Search by name or email"
             value={search}
             onChange={(e)=>setSearch(e.target.value)}
-            className="border border-gray-400 rounded-md px-4 py-2 mr-4 transition-all duration-200 focus:outline-black focus:ring-blue-500"
+            className="border border-gray-400 rounded-md px-4 py-2 mr-4 focus:outline-black focus:ring-blue-500"
             />
         </>
     )
