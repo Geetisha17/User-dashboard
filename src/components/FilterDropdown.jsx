79a9226924dd9fd3368c5d06aec78bbc0d72ad5f
Geetisha Tandon <93433422+Geetisha17@users.noreply.github.com>
2025-10-19 16:57:09 +0530

    Update FilterDropdown.jsx

--- src/components/FilterDropdown.jsx
+++ src/components/FilterDropdown.jsx
@@ -15,7 +15,7 @@ export default function Filterdropdown()
             <select
                 value={companyName}
                 onChange={(e)=>setCompanyName(e.target.value)}
-                className="px-5 py-2 border border-gray-300 rounded-md bg-white focus-outline-black transition-all duration-200"
+                className="px-5 py-2 border border-gray-300 rounded-md bg-white focus-outline-black"
             >
                 <option value="">All Compaines</option>
                 {uniqueCompanyNames.map((name)=>(
